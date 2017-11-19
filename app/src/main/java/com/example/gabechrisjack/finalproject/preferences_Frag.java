2f3048588ec8b155cf1d2f497c95e7e0c49fdef4
cmoorad <cmoorad@gmail.com>
2017-11-19 15:49:05 -0500

    last update

--- app/src/main/java/com/example/gabechrisjack/finalproject/preferences_Frag.java
+++ app/src/main/java/com/example/gabechrisjack/finalproject/preferences_Frag.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
  * Created by Chris Moorad on 10/10/17.
  */
 
+//this fragment is currently functionless, but still exists to be built out in future if desired
+//the ranking fragment we left out completely, following lab 3
+
 public class preferences_Frag extends Fragment {
 
     View v;
